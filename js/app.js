cbf680080bd83d370940e0295ceb5c73539a4a24
raul perez <raul@jonmi06.saregune>
2023-01-18 12:02:11 +0100

    Añadida opción de eliminar item de localStorage

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 const btnOpen = document.querySelector(".add-new")
 const btnClose = document.querySelector(".close")
 const addTask = document.querySelector(".add")
+const deleteTask = document.querySelector(".delete")
 const box = document.querySelector(".form")
 const tasksTable = document.querySelector(".tasksTable")
 
@@ -54,24 +55,40 @@ const addTaskArray = (e) => {
     addTaskDOM(allTaskRecover)
 }
 
+const deleteTaskArray =(index) =>{
+    let allTaskRecover = JSON.parse(localStorage.getItem('allTask')); 
+	console.log("indice del elemento:" +index);		
+    //remove matched item index
+    allTaskRecover.splice(index, 1);
+    localStorage.setItem('allTask', JSON.stringify(allTaskRecover));
+    console.log(allTaskRecover)
+    addTaskDOM(allTaskRecover)
+}
+
 const addTaskDOM = (allTask) => {
     console.log('Entramos en funcion addTaskDom')
-console.log(allTask)
-const tasks = allTask.map(function(item){
-        return `
-        <div class="task-single">
-            <div>
-                <p>${item.datetask}</p>
-            </div>
-            <div>
-                <p>${item.title}</p>
-                <p>${item.description}</p>
-            </div>
-        </div>`
+    console.log(allTask)
+    const tasks = allTask.map(function(item,index){
+        console.log("Mostrando item")
+        console.log(item)
+        console.log("index ="+ index)
+
+            return `
+            <div class="task-single">
+            <p onclick="deleteTaskArray(${index})" class="delete"></p>
+                <div>
+                    <p>${item.datetask}</p>
+                </div>
+                <div>
+                    <p>${item.title}</p>
+                    <p>${item.description}</p>
+                </div>
+            </div>`
     });
     console.log(tasks)
     tasksTable.innerHTML = tasks
 }
+
 
 btnOpen.addEventListener('click', (e) => {
     e.preventDefault()
@@ -82,6 +99,8 @@ btnClose.addEventListener('click', (e) => {
     e.preventDefault()
     box.style.display = 'none'
 });
+
+
 
 /* Add task */
 
@@ -98,8 +117,7 @@ window.addEventListener('DOMContentLoaded', () => {
         const allTaskStr= localStorage.getItem("allTask")
         const allTask = JSON.parse(allTaskStr);
         console.log(allTask)
-        addTaskDOM(allTask)
-        
+        addTaskDOM(allTask)     
     }
     
 });
